bf1a9548cd2fe14e9a8371391c3563c052fb3c73
PrernaJain1 <jain.mini2509@gmail.com>
2026-04-29 16:21:18 +0530

    Hashtable code

---
+++ src/main/java/org/example/Maps/HashtablePractice.java
@@ -0,0 +1,27 @@
+package org.example.Maps;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+public class HashtablePractice {
+    public static void main(String[] args){
+        //Hashtable doesn't allow null key or value   --> Hashmap allow one null as key and value any number of null
+        //Hashtable Synchronized  -->  hashmap not synchronized
+        //hashtable slower than hashmap
+        //Legacy class
+        //hashtable thread safe --> hashmap not thread safe
+        Hashtable<Integer, String> ht = new Hashtable<>();
+        ht.put(1,"Orange");
+        ht.put(2,"Apple");
+        ht.put(3,"Mango");
+        System.out.println(ht);
+        ht.remove(2);
+        System.out.println(ht);
+        System.out.println(ht.contains("Orange"));
+
+        for(Map.Entry<Integer,String> s: ht.entrySet()){
+            System.out.println(s.getKey());
+        }
+    }
+
+}
